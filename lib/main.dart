942ee53e48650a19f6dbf12d0d3c4d3a5932f1e4
Arkadiusz Antonkiewicz <arkadiusz.antonkiewicz@op.pl>
2024-08-09 10:44:41 +0200

    Instalacja Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_application_2/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
